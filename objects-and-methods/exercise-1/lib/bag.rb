ef0642683e2d218bf84681e670ea4b395c6757c0
Kelsie Porter <krporter22@gmail.com>
2022-09-21 21:50:29 -0600

    Complete bag class

---
+++ objects-and-methods/exercise-1/lib/bag.rb
@@ -0,0 +1,33 @@
+class Bag
+  attr_accessor :candies
+  def initialize
+    @candies = []
+  end
+
+  def empty?
+    if @candies.size >= 1
+      false
+    else
+      true
+    end
+  end
+
+  def count
+    @candies.size
+  end
+
+
+  def place_candy(a_candy)
+    @candies << a_candy
+  end
+
+  def contains?(type_of_candy)
+    @candies.each do |candy|
+      if candy.type == type_of_candy
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
